1c430704f41b06cb7e6f18f70b8671cae92c9ab3
marcelo cerqueira <marcelocerqueira661@gmail.com>
2022-04-29 23:07:03 -0300

    refatorando

--- Stage-05/script.js
+++ Stage-05/script.js
@@ -1,29 +1,35 @@
-// Dom
-// document object model
-  
-// refatoração: mudar um código para deixá-lo mais entedível
-// deixa o código mais performático
-// SEM ALTERAR SUAS FUCIONALIDADES
-
-
-
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
 const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
+let minutes
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
 
-// Event-driven
-// programação imperativa
-// callback
+function countdown(){
+  setTimeout(function() {
+    let seconds =  Number(secondsDisplay.textContent)
+
+    if( seconds <= 0 ) {
+      seconds = 60
+    }
+
+    secondsDisplay.textContent = seconds - 1
+    
+    countdown()
+  }, 1000)
+}
 
 buttonPlay.addEventListener('click', function() {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
-  
+
+  countdown()
+
 })
 
 buttonPause.addEventListener('click', function() {
@@ -36,17 +42,19 @@ buttonStop.addEventListener('click', function() {
   buttonPause.classList.add('hide')
   buttonSet.classList.remove('hide')
   buttonStop.classList.add('hide')
-  
 })
 
 buttonSoundOff.addEventListener('click', function() {
   buttonSoundOn.classList.remove('hide')
   buttonSoundOff.classList.add('hide')
-
 })
 
 buttonSoundOn.addEventListener('click', function() {
   buttonSoundOn.classList.add('hide')
   buttonSoundOff.classList.remove('hide')
+})
 
+buttonSet.addEventListener('click', function() {
+  minutes = prompt('Quantos minutos?')
+  minutesDisplay.textContent = minutes
 })
