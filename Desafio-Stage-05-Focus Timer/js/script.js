b5083e5c4f8a46ee9881c561b9da01fe0d8da7e1
marcelo cerqueira <marcelocerqueira661@gmail.com>
2022-05-11 22:33:40 -0300

    add

--- Desafio-Stage-05-Focus Timer/js/script.js
+++ Desafio-Stage-05-Focus Timer/js/script.js
@@ -1,6 +1,8 @@
 import Sound from "./sounds.js"
 const sound = Sound()
 
+
+
 const buttonPlay = document.querySelector('.play')
 const buttonPause  = document.querySelector('.pause')
 const buttonStop  = document.querySelector('.stop')
